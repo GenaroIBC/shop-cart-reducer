9cf092e16b02fb2bddde6b7171a8ea96629208db
genaroibc <bonavitaismael@gmail.com>
2022-09-10 09:36:11 -0300

    feat: add ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+export function ProductCard({ data, addToCart }) {
+  const { id, title, price } = data;
+  return (
+    <article className="product-card">
+      <h2>{title}</h2>
+      <h3>{price}</h3>
+      <h4>{id}</h4>
+      {/* <button onClick={console.log}></button> */}
+      <button onClick={() => addToCart(id)}>Add</button>
+    </article>
+  );
+}
