2e118f22bc73d383ba56699072c513421fc7f3c9
genaroibc <bonavitaismael@gmail.com>
2022-09-10 10:50:13 -0300

    feat: show cart products in the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useReducer } from "react";
+import { cartReducer, INITIAL_CART } from "./reducers/cartReducer";
+import "./App.css";
+import { ProductCard } from "./components/ProductCard";
+import { CART_TYPES } from "./actions/cartActions";
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [cartState, cartDispatch] = useReducer(cartReducer, INITIAL_CART);
+
+  const addToCart = id => {
+    cartDispatch({ type: CART_TYPES.ADD_TO_CART, payload: id });
+  };
+
+  console.log(cartState);
+  return (
+    <>
+      <div className="products-container">
+        {cartState.products.map(product => (
+          <ProductCard addToCart={addToCart} key={product.id} data={product} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
